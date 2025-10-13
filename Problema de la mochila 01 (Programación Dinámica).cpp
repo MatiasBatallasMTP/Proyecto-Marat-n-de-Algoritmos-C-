55a0cfd6c10846aa2f90a2fdc81d09cd341d9408
Matias2149 <mbatallas@alumno.montepiedra.edu.ec>
2025-10-13 13:31:14 -0500

    Create Problema de la mochila 01 (Programación Dinámica).cpp

---
+++ Problema de la mochila 01 (Programación Dinámica).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int knapsack(vector<int>& weights, vector<int>& values, int W) {
+    int n = weights.size();
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int w = 1; w <= W; w++) {
+            if (weights[i - 1] <= w) {
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
+            } else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+    return dp[n][W];
+}
+
+int main() {
+    vector<int> weights = {1, 2, 3};
+    vector<int> values = {10, 20,
