ed074a6a82b9f2e1da281adc23f7260a9b678921
Matias2149 <mbatallas@alumno.montepiedra.edu.ec>
2025-10-28 19:36:17 -0500

    Add Dutch national flag problem implementation
    Implement Dutch national flag problem using a 3-way partitioning algorithm.

---
+++ Dutch national flag (3-way partition).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void dutchFlag(vector<int>& nums){
+    int low=0, mid=0, high=nums.size()-1;
+    while(mid<=high){
+        if(nums[mid]==0) swap(nums[low++], nums[mid++]);
+        else if(nums[mid]==1) mid++;
+        else swap(nums[mid], nums[high--]);
+    }
+}
+
+int main(){
+    vector<int> nums={2,0,2,1,1,0};
+    dutchFlag(nums);
+    for(int x:nums) cout<<x<<" "; cout<<endl;
+}
