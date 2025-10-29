c259dfc0a0fea9e2115047a583f058ee40a6dd03
Matias2149 <mbatallas@alumno.montepiedra.edu.ec>
2025-10-28 19:33:16 -0500

    Implement max product subarray function

---
+++ Subarray product max.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxProduct(vector<int>& nums){
+    int maxP=nums[0], minP=nums[0], res=nums[0];
+    for(int i=1;i<nums.size();i++){
+        if(nums[i]<0) swap(maxP,minP);
+        maxP=max(nums[i], maxP*nums[i]);
+        minP=min(nums[i], minP*nums[i]);
+        res=max(res,maxP);
+    }
+    return res;
+}
+
+int main(){
+    vector<int> nums={2,3,-2,4};
+    cout << "Max product: " << maxProduct(nums) << endl;
+}
